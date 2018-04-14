9b28986bcf1ef6230f80983fa671e91729f704fa
andychen <andy.chen@iisigroup.com>
2018-04-14 23:04:12 +0800

    [APP] modify spin result toString

--- src/main/java/com/genesis/exams/happyslot/model/HappySpinResult.java
+++ src/main/java/com/genesis/exams/happyslot/model/HappySpinResult.java
@@ -4,7 +4,8 @@ import com.genesis.exams.slot.SpinResult;
 import com.genesis.exams.slot.Symbol;
 
 /**
- *  A SpinResult extends genesis SpinResult
+ * A SpinResult extends genesis SpinResult
+ *
  * @author AndyChen
  * @version <ul>
  *          <li>2018/4/14 AndyChen,new
@@ -14,5 +15,19 @@ import com.genesis.exams.slot.Symbol;
 public class HappySpinResult extends SpinResult {
     public HappySpinResult(Symbol[] symbols, long payout) {
         super(symbols, payout);
+    }
+
+    @Override
+    public String toString() {
+        if (super.getSymbols() == null)
+            return super.toString();
+        StringBuilder stringBuilder = new StringBuilder();
+        Symbol[] symbols = super.getSymbols();
+        for (int i = 0; i < symbols.length; i++) {
+            stringBuilder.append(symbols[i].getName());
+            if (i < symbols.length - 1)
+                stringBuilder.append(",");
+        }
+        return stringBuilder.toString();
     }
 }
